59a301110c89b55cc41881152492e989e9ae5e74
Никита Волков <iallknow1@yandex.ru>
2023-04-03 23:23:36 +0300

    Fixed mistakes

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,6 +11,6 @@ router.register('v1/follow', FollowViewSet, basename='follows')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
